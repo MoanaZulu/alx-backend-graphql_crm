90507cb83843370c0c9797b6af5b739f0b572c40
Moana <122973439+MoanaZulu@users.noreply.github.com>
2025-12-13 15:00:29 +0200

    Update schema.py

--- crm/schema.py
+++ crm/schema.py
@@ -12,3 +12,25 @@ class Query(graphene.ObjectType):
 
     def resolve_all_customers(root, info):
         return Customer.objects.all()
+
+
+import graphene
+from graphene_django import DjangoObjectType
+from django.core.exceptions import ValidationError
+from .models import Customer, Product, Order
+
+# --- GraphQL Types ---
+class CustomerType(DjangoObjectType):
+    class Meta:
+        model = Customer
+        fields = ("id", "name", "email", "phone")
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = ("id", "name", "price", "stock")
+
+class OrderType(DjangoObjectType):
+    class Meta:
+        model = Order
+        fields = ("id", "customer", "products", "total_amount", "order_date")
